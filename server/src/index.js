c8d4fc9946b143d771b3fde7ac28881d515f865d
Saba Kathawala <sabaskathawala@gmail.com>
2022-08-09 11:57:05 -0700

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,18 @@
-//TODO
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema');
+const mockedData = require('./mocks/mockedData');
+
+const server = new ApolloServer({
+    typeDefs: typeDefs,
+    // This instructs Apollo Server to populate every queried schema field with a
+    // placeholder value (such as Hello World for String fields).
+    mocks: mockedData
+});
+
+server.listen().then(() => {
+    console.log(`
+        🚀 Server is running!
+        🔉  Listening on port 4000
+        📭  Query at "http://localhost:4000"
+    `)
+});
